40011ee79434280ecf91b2c0ffe06cf8986ef6fc
Phunsukwangdu <susmit9370@gmail.com>
2016-09-06 02:05:12 +0530

    linked list practice

---
+++ linkedlist.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+struct node{
+    int index;
+    struct node *next;
+};
+
+typedef struct node node;
+node *head=NULL;
+
+void insert(int a){
+    node *temp=(node *)malloc(sizeof(struct node));
+    temp->index=a;
+    temp->next=head;
+    head=temp;
+}
+
+void prnt(){
+    node *temp=(node *)malloc(sizeof(struct node));
+    temp=head;
+    printf("\n");
+    while(temp){
+        printf("%d->",temp->index);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+
+
+int main(){
+insert(1);
+insert(2);
+insert(3);
+insert(4);
+insert(5);
+prnt();
+
+
+}
